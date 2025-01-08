c4bd655c71346dd8905bebb607ed32c611c82685
murifaTAS <amrock900@gmail.com>
2025-01-08 10:11:27 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# twozerofoureight_flutter
+ 2048 Flutter game made by Adam
---
+++ android/app/src/main/kotlin/com/twozerofoureight/adam/twozerofoureight_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.twozerofoureight.adam.twozerofoureight_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
