8c61703180bc1c915f3dd01d9d3c232c27ebef9f
murifaTAS <amrock900@gmail.com>
2025-01-08 10:13:18 +0800

    Added version control

---
+++ lib/version_control.dart
@@ -0,0 +1,9 @@
+class VersionController {
+  static String Ver = "Ver 0.0.1";
+  static int Build = 0;
+  static String ApkRelease = "r1";
+  static String EditedBy = "adam";
+  static String Date = "0812025 10:13AM";
+//flutter run -d chrome --web-renderer html --web-browser-flag  "--disable-web-security"
+}
+
